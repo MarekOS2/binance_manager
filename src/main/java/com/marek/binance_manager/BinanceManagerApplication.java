a08beb1433cf6db31dd1d59da823f4d7dc14d667
Marek Kulczyk <pz2.kulczykmarek@gmail.com>
2023-06-09 08:56:31 +0200

    initial commit

---
+++ src/main/java/com/marek/binance_manager/BinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.marek.binance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinanceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marek/binance_manager/BinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marek.binance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
